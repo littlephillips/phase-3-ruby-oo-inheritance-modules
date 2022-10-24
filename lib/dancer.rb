96bd93a2ffd12ebb75158e4874018681b58a48b4
littlephillips <phillipkinuthia72@gmail.com>
2022-10-24 12:04:33 +0300

    Inheritance module

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,25 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+#with namespaced modules
+
+# require_relative './fancy_dance.rb'
+# class Dancer
+#     extend FancyDance::ClassMethods
+#     include FancyDance::InstanceMethods
+#     attr_accessor :name
+
+#     def initialize(name)
+#       @name = name
+#     end
+# end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,24 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+# require_relative './fancy_dance.rb'
+# class Kid
+#     extend FancyDance::ClassMethods
+#     include FancyDance::InstanceMethods
+#     attr_accessor :name
+
+#     def initialize(name)
+#       @name = name
+#     end
+# end
